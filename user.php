2b1e340166ee47a97feec037f0f10aac106bbd97
Simon de Wendt <simon.dewendt@cec.valantic.com>
2023-08-09 14:08:03 +0200

    Registration (not done yet just for backup)

--- user.php
+++ user.php
@@ -4,30 +4,63 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-if (isset($_POST["username"], $_POST["mail"], $_POST["password"])) {
+$error = null;
 
-    $user = $_POST["username"];
-    $eMail = $_POST["mail"];
+if (!file_exists("user.json")) {
+    file_put_contents("user.json", json_encode([]));
+}
+
+$user = json_decode(file_get_contents("user.json"), true);
+
+function validatePassword($password): bool
+{
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number = preg_match('@[0-9]@', $password);
+    $specialChar = preg_match('@[^\w]@', $password);
+
+    $minLength = 6;
+
+    return $uppercase && $lowercase && $number && $specialChar && strlen($password) >= $minLength;
+}
+
+if (isset($_POST["username"], $_POST["mail"], $_POST["password"])) {
+    $userName = $_POST["username"];
+    $eMailCheck = $_POST["mail"];
     $password = $_POST["password"];
 
-    $newUser = array(
-        "user" => $user,
-        "eMail" => $eMail,
-        "password" => $password,
-    );
+    if (!empty($user)) {
+        foreach ($user as $userData) {
+            if ($userData["eMail"] === $eMailCheck) {
+                $error = "Fehler eMail bereits vergeben";
+                break;
+            }
+        }
+    }
 
-    if (file_exists("account.json") === false) {     //First entry in transaction log?
-        $UserToSave = array($newUser);
+    if (filter_var($eMailCheck, FILTER_VALIDATE_EMAIL)) {
+        $eMail = $eMailCheck;
+    }
+
+    if (validatePassword($password) === true) {
+        if (!isset($error)) {
+            $newUser = [
+                "user" => $userName,
+                "eMail" => $eMail,
+                "password" => $password,
+            ];
+
+            $user[] = $newUser;
+        }
     } else {
-        $oldUsers = json_decode(file_get_contents("user.json"), false);
-        $UserToSave = $oldUsers;
+        $error = "Passwort Anforderungen nicht erfüllt (find out yourself)";
     }
+}
 
-    if (!file_put_contents("user.json", json_encode($UserToSave, JSON_PRETTY_PRINT), LOCK_EX)) {
-        $error = "Fehler!";
-    }else{
-        $success = "Erfolg!";
-    }
+if (!isset($error) && file_put_contents("user.json", json_encode($user, JSON_PRETTY_PRINT), LOCK_EX)) {
+    echo "yes";
+}else{
+    echo $error;
 }
 
 include 'registration.php';

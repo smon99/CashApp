9ccc7f9c0197c0022880def1eaee62b219127727
Simon de Wendt <simon.dewendt@cec.valantic.com>
2023-08-10 12:13:42 +0200

    Registrierung

--- user.php
+++ user.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -13,7 +14,7 @@ if (!file_exists("user.json")) {
 
 $user = json_decode(file_get_contents("user.json"), true);
 
-function validatePassword($passwordCheck): bool     //validate if password criteria is met
+function validatePassword($passwordCheck): bool
 {
     $uppercase = preg_match('@[A-Z]@', $passwordCheck);
     $lowercase = preg_match('@[a-z]@', $passwordCheck);
@@ -27,6 +28,7 @@ function validatePassword($passwordCheck): bool     //validate if password crite
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (empty($_POST["username"]) || empty($_POST["mail"]) || empty($_POST["password"])) {
         $error = "Alle Felder müssen ausgefüllt sein!";
+
         if (isset($_POST["username"])) {
             $tempUserName = $_POST["username"];
         }
@@ -36,49 +38,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST["password"])) {
             $tempPassword = $_POST["password"];
         }
-    }
-} elseif (isset($_POST["username"], $_POST["mail"], $_POST["password"])) {
+    } elseif (isset($_POST["username"], $_POST["mail"], $_POST["password"])) {
 
-    $userName = $_POST["username"];
-    $eMailCheck = $_POST["mail"];
-    $passwordCheck = $_POST["password"];
+        $userName = $_POST["username"];
+        $eMailCheck = $_POST["mail"];
+        $passwordCheck = $_POST["password"];
 
-    if (!empty($user)) {
-        foreach ($user as $userData) {
-            if ($userData["eMail"] === $eMailCheck) {
-                $error = "Fehler eMail bereits vergeben";
-                break;
+        if (!empty($user)) {
+            foreach ($user as $userData) {
+                if ($userData["eMail"] === $eMailCheck) {
+                    $error = "Fehler eMail bereits vergeben";
+                    break;
+                }
             }
         }
-    }
 
-    if (filter_var($eMailCheck, FILTER_VALIDATE_EMAIL)) {
-        $eMail = $eMailCheck;
-    }
-
-    if (validatePassword($passwordCheck) === true) {
-
-        $password = password_hash($passwordCheck, PASSWORD_DEFAULT);
-
-        if (!isset($error)) {
-            $newUser = [
-                "user" => $userName,
-                "eMail" => $eMail,
-                "password" => $password,
-            ];
-
-            $user[] = $newUser;
+        if (filter_var($eMailCheck, FILTER_VALIDATE_EMAIL)) {
+            $eMail = $eMailCheck;
         }
+        if (!isset($eMail)) {
+            $error = "Bitte gültige eMail eingeben!";
+        }
+
+        if (validatePassword($passwordCheck)) {
+
+            $password = password_hash($passwordCheck, PASSWORD_DEFAULT);
+
+            if (!isset($error)) {
+                $newUser = [
+                    "user" => $userName,
+                    "eMail" => $eMail,
+                    "password" => $password,
+                ];
+
+                $user[] = $newUser;
+            }
+        } else {
+            $error = "Passwort Anforderungen nicht erfüllt (find out yourself)";
+        }
+    }
+
+    if (!isset($error)) {
+        echo "yeah";
+        file_put_contents("user.json", json_encode($user, JSON_PRETTY_PRINT));
     } else {
-        $error = "Passwort Anforderungen nicht erfüllt (find out yourself)";
+        echo $error;
     }
 }
-
-if (!isset($error)) {
-    file_put_contents("user.json", json_encode($user, JSON_PRETTY_PRINT));
-    echo 'Yeah';
-} else {
-    echo $error;
-}
-
 include 'registration.php';
